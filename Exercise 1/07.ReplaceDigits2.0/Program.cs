f3cbcf5adf600739608f2308f7c17a1a531f0450
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-22 22:07:52 +0200

    new

---
+++ Exercise 1/07.ReplaceDigits2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _07.ReplaceDigits2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ch = Console.ReadLine(); 
+            for (int i = 0; i < ch.Length; i++)
+            {
+                int number = int.Parse(ch[i].ToString());
+                Console.Write(number + " ");
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
