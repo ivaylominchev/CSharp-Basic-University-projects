e60d2a47800e5b0e73262b00f63e1b877586d637
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-22 22:07:28 +0200

    new

---
+++ Exercise 1/05.FromulaMatch/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _05.FromulaMatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int x = 1; x <= n; x++)
+            {
+                for (int y = 1; y <= n; y++)
+                {
+                    for (int z = 1; z <= n; z++)
+                    {
+                        if (x * x + y * y == z * z * z)
+                        {
+                            Console.WriteLine($"{x}^2+{y}^2={z}^3");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
