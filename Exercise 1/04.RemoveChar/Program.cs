a0a9c0da9ab43ff8e8680c41c2bc1ea672f5f2f4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-22 22:07:15 +0200

    new

---
+++ Exercise 1/04.RemoveChar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.RemoveChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ch)
+                {
+
+                    int index = text.IndexOf(ch);
+                    text = text.Remove(index, 1);
+
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
