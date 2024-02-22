a18e770f3b7ec51522ab9b05b195537e2b46a54c
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-22 22:06:34 +0200

    new

---
+++ Exercise 1/03.Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
