e3e5904916e010a05bd96fa6bbbc4fa8fd33f5cb
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-22 22:07:40 +0200

    new

---
+++ Exercise 1/06.ReplaceDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _06.ReplaceDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                int lastnumber = n % 10;
+                Console.Write(lastnumber + " ");
+                for (int j = 0; j < lastnumber; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                n /= 10;
+            }
+        }
+    }
+}
