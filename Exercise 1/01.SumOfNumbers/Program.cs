b1ff7ee9bc519db580623d2fb57464879b213a06
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-22 22:06:03 +0200

    new

---
+++ Exercise 1/01.SumOfNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
