0fea09157d5c712f073f3d926b670d14350303c8
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-28 23:46:08 +0200

    add

---
+++ Homework 1/02. Average and Max/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _02._Average_and_Max
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double average = 0;
+            int max = int.MinValue;
+            double count = 0;
+            
+            while (number != 0)
+            {
+                average += number;
+                if (number > max)
+                {
+                    max = number;
+                }
+                count++;
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(average/count);
+            Console.WriteLine(max);
+        }
+    }
+}
