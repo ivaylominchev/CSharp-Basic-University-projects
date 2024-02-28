3b8b359acf7ba8d728739cb95ae015d7e8d83c72
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-28 23:46:27 +0200

    add

---
+++ Homework 1/03. Two-digit Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+
+namespace _03._Two_digit_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = int.Parse(Console.ReadLine());
+
+            
+
+            for (int i = 10; i <= 99 ; i++)
+            {
+                int firstDigit = i / 10;
+                int secondDigit = i % 10;
+                
+
+                if (digit == firstDigit || digit == secondDigit)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
