3353d1c3a4c0f90068cf3ce05b24e7583759c818
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-28 23:45:48 +0200

    add

---
+++ Homework 1/01. Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int count= 0;
+
+            foreach (var item in name.ToLower())
+            {
+                if (item == 'а' 
+                    || item == 'е' 
+                    || item == 'и' 
+                    || item == 'о' 
+                    || item == 'у' 
+                    || item == 'ъ' 
+                    || item == 'ю' 
+                    || item == 'я')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
