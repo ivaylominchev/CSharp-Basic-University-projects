6f437fea87c47daed478a5167b0137a35b7ce1e5
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:59:22 +0200

    new add

---
+++ Exercise 2/03. Prime number 2.0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _03._Prime_number_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter number");
+            int.TryParse(Console.ReadLine(), out number);
+            bool check = true;
+            if (number == 0 || number == 1)
+            {
+                check = false;
+            }
+            else
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    Console.WriteLine($"{number} is prime number");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is not prime number");
+                }
+            }
+        }
+    }
+}
