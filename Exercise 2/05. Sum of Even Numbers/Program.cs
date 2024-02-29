08872efe5b7ef2b82f36b8eb35b8cef4b8f39620
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:59:54 +0200

    new add

---
+++ Exercise 2/05. Sum of Even Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05._Sum_of_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
