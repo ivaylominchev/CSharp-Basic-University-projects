e0148949f78abbd4e33b91af6b11ba283f5e04b4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:59:43 +0200

    new add

---
+++ Exercise 2/04. Correct number/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Correct_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            while (number < 5 || number > 50 )
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(Math.Pow(number, 2));
+        }
+    }
+}
