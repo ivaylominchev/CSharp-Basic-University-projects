5b649b233f6e36947b6e648a168ea350e5301c7a
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:58:46 +0200

    new add

---
+++ Exercise 2/02. Leap year Check/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Timers;
+
+namespace _02._Leap_year_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine($"{year} is Leap year");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not Leap year");
+            }
+        }
+        
+    }
+}
