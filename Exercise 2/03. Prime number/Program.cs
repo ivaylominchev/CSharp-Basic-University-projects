a3c00aef23a3f3e03eba8151ad30c8ed55521d2d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:59:04 +0200

    new add

---
+++ Exercise 2/03. Prime number/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _03._Prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+
+            if (number == 0 || number == 1)
+            {
+                Console.WriteLine(number + " is not prime number");
+                return;
+            }
+            for (int i = 1; i <= number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine($"{number} is not prime number");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is prime number");
+                }
+            }
+            /*if (number == 0 || number == 1)
+            {
+                Console.WriteLine($"{number} is not prime number");
+            }
+            else
+            {
+                for (int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        Console.WriteLine(number + " is not prime number");
+                        return;
+                    }
+                }
+                Console.WriteLine($"{number} is prime number");
+            }*/
+        }
+    }
+}
