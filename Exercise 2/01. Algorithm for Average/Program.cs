a4dc05fe74bbe9c4ffa91d4543d70e973a2a05ce
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:58:08 +0200

    new add

---
+++ Exercise 2/01. Algorithm for Average/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01._Algorithm_for_Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0; int b=0; int c=0;
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            int.TryParse(Console.ReadLine(), out c);
+
+            decimal average = (a + b + c) / 3;
+
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
