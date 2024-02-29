f3abc84c5e08aa042a74f811bcb7f832f18ede6a
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-02-29 09:58:24 +0200

    new add

---
+++ Exercise 2/02. Leap year 2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Leap_year_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Въведете година: ");
+            int.TryParse(Console.ReadLine(), out year);
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine($"{year} е високосна");
+            }
+            else
+            {
+                Console.WriteLine($"{year} не е високосна");
+            }
+        }
+    }
+}
