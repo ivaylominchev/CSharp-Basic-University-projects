d26b1ab08fe01e3b00fd1ffff759a776816d2cc7
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 12:44:34 +0200

    finish project

--- Exercise 3/06. Subarray/Program.cs
+++ Exercise 3/06. Subarray/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] firstArr = new int[10];
+            int[] secondArr = new int[3];
+            Console.WriteLine("Enter first array: ");
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                firstArr[i] = int.Parse(Console.ReadLine());   
+            }
+            Console.WriteLine("Enter second array: ");
+            for (int i = 0; i < secondArr.Length; i++)
+            {
+                secondArr[i] = int.Parse(Console.ReadLine());
+            }
+            
+            bool subArr = !secondArr.Except(firstArr).Any();
+            Console.WriteLine("Subarray: " + subArr);
         }
     }
 }
