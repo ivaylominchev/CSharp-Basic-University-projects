f9836b8843d319addf5c6db470aea509f34db031
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 10:35:50 +0200

    Update Program.cs

--- Exercise 3/01. Array Average/Program.cs
+++ Exercise 3/01. Array Average/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = new int[10];
+            double average = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int.TryParse(Console.ReadLine(), out array[i]);
+                average += array[i];
+            }
+
+            average = Queryable.Average(array.AsQueryable());
+            Console.WriteLine(average);
+            //average = array.Average();
         }
     }
 }
