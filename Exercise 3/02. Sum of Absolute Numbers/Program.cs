bc74f2e91b23175e2240b3937682d03bd45bba12
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 10:57:21 +0200

    finish project

--- Exercise 3/02. Sum of Absolute Numbers/Program.cs
+++ Exercise 3/02. Sum of Absolute Numbers/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter array size: ");
+            int m = int.Parse(Console.ReadLine());
+            int[] arr = new int[m];
+            int sum = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                arr[i] = int.Parse(Console.ReadLine());
+                arr[i] = Math.Abs((int)arr[i]);
+                sum += arr[i];
+            }
+            
+            Console.WriteLine("Sum: " + sum);
         }
     }
 }
