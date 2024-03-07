79d372d4255e66e288c7226e2b3a931425474e8b
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 12:28:54 +0200

    new project

---
+++ Exercise 3/06. Subarray/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Subarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
