e7212de80c9808533325a32fedd46927458dec89
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 11:23:28 +0200

    create project

---
+++ Exercise 3/04. Most Common Symbol/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04._Most_Common_Symbol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
