d85edddc536b0973c82f87ecc5426711f96dc3b8
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 10:46:04 +0200

    create project

---
+++ Exercise 3/02. Sum of Absolute Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Sum_of_Absolute_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
