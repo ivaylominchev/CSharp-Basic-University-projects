c6b9f45eacfc775b886fabe23d49a6204a251b8c
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-07 10:12:36 +0200

    create project

---
+++ Exercise 3/01. Array Average/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Array_Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
