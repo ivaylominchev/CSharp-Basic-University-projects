c09529158ff56e12fd59672409a353b980643994
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-14 08:39:57 +0200

    create

---
+++ Exercise 4/01. Student Info/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Student_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
