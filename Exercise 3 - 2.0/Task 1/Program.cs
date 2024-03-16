fc65267ccb97c7770500d7813f14e12aae588365
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-16 23:13:32 +0200

    Update Program.cs

--- Exercise 3 - 2.0/Task 1/Program.cs
+++ Exercise 3 - 2.0/Task 1/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,] A = new int[3, 3]; 
+            for (int i = 0; i < A.GetLength(0); i++) 
+            { 
+                for (int j = 0; j < A.GetLength(1); j++) 
+                { Console.Write($"A[{i},{j}]="); 
+                    int.TryParse(Console.ReadLine(), out A[i, j]); 
+                } 
+            }
+            int sum = 0; 
+            int count = 0; 
+            foreach (int element in A) 
+            {
+                if (element > 0)
+                {
+                    sum += element;
+                    count++;
+                } 
+            }
+            Console.WriteLine("Average is {0:0.00}", (decimal)sum/count);
         }
     }
 }
