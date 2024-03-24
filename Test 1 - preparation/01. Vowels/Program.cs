9ee64dbc3199098eaad1549b5eff33c90fb03448
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-24 20:03:22 +0200

    Update Program.cs

--- Test 1 - preparation/01. Vowels/Program.cs
+++ Test 1 - preparation/01. Vowels/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string word = Console.ReadLine().ToLower();
+            int count = 0;
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'а' || 
+                    word[i] == 'е' || 
+                    word[i] == 'и' || 
+                    word[i] == 'о' || 
+                    word[i] == 'у' || 
+                    word[i] == 'ъ' || 
+                    word[i] == 'ю' || 
+                    word[i] == 'я')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
