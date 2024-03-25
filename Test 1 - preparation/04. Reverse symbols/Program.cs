d8ab0fc11153dfcf45a06875a84b30e20cfdcdbc
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-25 23:56:48 +0200

    Update Program.cs

--- Test 1 - preparation/04. Reverse symbols/Program.cs
+++ Test 1 - preparation/04. Reverse symbols/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            string words = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                words += symbol;
+            }
+            Console.WriteLine(string.Join(" ",words.Reverse()));
         }
     }
 }
