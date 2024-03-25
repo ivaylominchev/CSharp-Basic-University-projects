4f80567508393642268b1ed43f9befb64fbc721b
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-25 23:48:07 +0200

    Update Program.cs

--- Test 1 - preparation/03. Two-digit numbers/Program.cs
+++ Test 1 - preparation/03. Two-digit numbers/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 10; i < 100; i++)
+            {
+                int firstDigit = i / 10;
+                int lastDigit = i % 10;
+                
+                if (number == firstDigit || number == lastDigit)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
