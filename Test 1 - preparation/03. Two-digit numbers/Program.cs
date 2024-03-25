bb14a2e90382793a151d8896b15a4ae1bce5cb98
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-25 23:42:32 +0200

    create

---
+++ Test 1 - preparation/03. Two-digit numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Two_digit_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
