dd0ccb7a8e267707cfc384bade3d75210b204e50
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 00:09:29 +0200

    Update Program.cs

--- Test 1 - preparation/05. Number of matches/Program.cs
+++ Test 1 - preparation/05. Number of matches/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            int[] numbers = new int[10];
+            int count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int newNumber = int.Parse(Console.ReadLine());
+                numbers[i] = newNumber;
+            }
+            for (int i = 0; i < numbers.Length;i++)
+            {
+                if (numbers[i] == number)
+                {
+                    count++;
+                }
+            }
+            if (count > 0)
+            {
+                Console.WriteLine($"Yes, {number} exists. The number occurs {count} times.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} does not exist.");
+            }
         }
     }
 }
