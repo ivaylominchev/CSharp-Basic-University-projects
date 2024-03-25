9e9c7e9b7534bb40cacb2d48c1535b3589d39e60
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-25 23:59:35 +0200

    create

---
+++ Test 1 - preparation/05. Number of matches/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Number_of_matches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
