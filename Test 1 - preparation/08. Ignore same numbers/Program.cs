772271e3a7dc49b35c2730f18aab34c86a16762e
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 18:57:48 +0200

    create

---
+++ Test 1 - preparation/08. Ignore same numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08._Ignore_same_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
