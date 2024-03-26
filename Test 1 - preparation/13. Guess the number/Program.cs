87be97b2001b2197ae1b8f204d40ef39221b5f38
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 22:41:06 +0200

    Update Program.cs

--- Test 1 - preparation/13. Guess the number/Program.cs
+++ Test 1 - preparation/13. Guess the number/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random rnd = new Random();
+            int number = rnd.Next(1,50);
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"№{i}: ");
+                int guessNumber = int.Parse(Console.ReadLine());
+                if (guessNumber < 1 || guessNumber > 50)
+                {
+                    Console.WriteLine("Out of bounds! Please enter new number between 1 and 50.");
+                    guessNumber = int.Parse(Console.ReadLine());
+                }
+
+                if (guessNumber == number)
+                {
+                    Console.WriteLine($"Congratulations, you guessed the number! The random number is {number}.");
+                    return;
+                }
+                else
+                {
+                    if (i < 5)
+                    {
+                        Console.WriteLine("Try again!");
+                    }
+                }
+            }
+            Console.WriteLine($"Sorry, you did not guess the number. The random number is {number}.");
         }
     }
 }
