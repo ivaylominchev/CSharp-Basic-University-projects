f52a74ccbf7be77c1ebaf8ae8399c441c92c58b5
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 20:07:03 +0200

    Update Program.cs

--- Test 1 - preparation/09. Even or odd numbers/Program.cs
+++ Test 1 - preparation/09. Even or odd numbers/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace _09._Even_or_odd_numbers
+﻿using System.Runtime.InteropServices;
+
+namespace _09._Even_or_odd_numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int countEven = 0;
+            int countOdd = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    countEven++;
+                }
+                else
+                {
+                    countOdd++;
+                }
+            }
+            if (countEven == 7)
+            {
+                Console.WriteLine("All numbers are even.");
+            }
+
+            if (countOdd == 7)
+            {
+                Console.WriteLine("All numbers are odd.");
+            }
         }
     }
 }
