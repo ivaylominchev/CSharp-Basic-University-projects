da22c19dbbc42d1b4d9948a8a5eeecba5e3d3561
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 19:40:11 +0200

    create

---
+++ Test 1 - preparation/09. Even or odd numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _09._Even_or_odd_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
