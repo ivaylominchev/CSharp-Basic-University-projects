57eafc4aae77a6692a85910a4855d5f96082e351
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 18:41:08 +0200

    create

---
+++ Test 1 - preparation/07. Contains digit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._Contains_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
