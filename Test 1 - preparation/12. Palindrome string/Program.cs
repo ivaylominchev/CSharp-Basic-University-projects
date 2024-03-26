96667f2434d10781ed4916ba3df5bcb10edacc13
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 21:41:35 +0200

    create

---
+++ Test 1 - preparation/12. Palindrome string/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12._Palindrome_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
