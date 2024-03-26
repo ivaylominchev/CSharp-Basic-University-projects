8c2a070b5c4e14ed68cce338e4ab89f8bcf7affb
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 22:02:20 +0200

    create

---
+++ Test 1 - preparation/13. Guess the number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13._Guess_the_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
