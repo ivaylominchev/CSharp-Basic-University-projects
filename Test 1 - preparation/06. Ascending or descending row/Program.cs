9ed8414b8bafb3d84291efa16fb83b9847f4fdf8
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 18:07:14 +0200

    create

---
+++ Test 1 - preparation/06. Ascending or descending row/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Ascending_or_descending_row
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
