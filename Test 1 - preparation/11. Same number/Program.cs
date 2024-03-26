4d21ad7c1b402fc35cccfb67473a825cc0703883
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 20:16:50 +0200

    create

---
+++ Test 1 - preparation/11. Same number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11._Same_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
