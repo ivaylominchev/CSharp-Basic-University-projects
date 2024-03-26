7629276e07f90a6da8f71897b95fc16f6cab7458
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 22:01:09 +0200

    Update Program.cs

--- Test 1 - preparation/12. Palindrome string/Program.cs
+++ Test 1 - preparation/12. Palindrome string/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            bool check = false;
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] == input[input.Length - 1 - i])
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+            }
+
+            if (check)
+            {
+                Console.WriteLine("The string is palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not palindrome.");
+            }
         }
     }
 }
