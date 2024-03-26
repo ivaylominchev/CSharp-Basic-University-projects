85d7cddbb0cffba44a6c611269c960a6b27fa763
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-26 19:34:16 +0200

    Update Program.cs

--- Test 1 - preparation/08. Ignore same numbers/Program.cs
+++ Test 1 - preparation/08. Ignore same numbers/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            int currentNumber = number;
+            double average = currentNumber;
+            int min = currentNumber;
+            
+            int count = 1;
+
+            for (int i = 0; i < 9; i++)
+            {
+                number = int.Parse((Console.ReadLine()));
+                if (currentNumber != number)
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                    average += number;
+                    count++;
+                }
+                currentNumber = number;
+            }
+            Console.WriteLine($"Average: {average/count:F2}, Min: {min}");
         }
     }
 }
