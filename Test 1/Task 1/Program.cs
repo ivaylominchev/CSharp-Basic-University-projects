86c1aa68e56df9b849699a031c5d714124570501
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-03-28 22:34:38 +0200

    Update Program.cs

--- Test 1/Task 1/Program.cs
+++ Test 1/Task 1/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                while (number < -9 || number > 9)
+                {
+                    Console.WriteLine("Enter number between -9 and 9!");
+                    number = int.Parse(Console.ReadLine());
+                }
+
+                array[i] = number;
+
+            }
+
+            Console.WriteLine("The array is: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string concat = array[i].ToString() + "1";
+                array[i] = int.Parse(concat);
+            }
+            Console.WriteLine("The final array is: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
