945a238d5356877714231619065ed6e4094eb6e4
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-12 01:43:41 +0300

    Update Form2.Designer.cs

--- C# Курсов проект/Курсов проект/Курсов проект/Form2.Designer.cs
+++ C# Курсов проект/Курсов проект/Курсов проект/Form2.Designer.cs
@@ -28,35 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DGrid2 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.DGrid2)).BeginInit();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Намери = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // DGrid2
+            // textBox1
             // 
-            this.DGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGrid2.Location = new System.Drawing.Point(42, 206);
-            this.DGrid2.Name = "DGrid2";
-            this.DGrid2.RowHeadersWidth = 51;
-            this.DGrid2.RowTemplate.Height = 24;
-            this.DGrid2.Size = new System.Drawing.Size(470, 150);
-            this.DGrid2.TabIndex = 0;
+            this.textBox1.Location = new System.Drawing.Point(82, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Намери
+            // 
+            this.Намери.Location = new System.Drawing.Point(354, 45);
+            this.Намери.Name = "Намери";
+            this.Намери.Size = new System.Drawing.Size(126, 42);
+            this.Намери.TabIndex = 2;
+            this.Намери.Text = "button1";
+            this.Намери.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.DGrid2);
+            this.Controls.Add(this.Намери);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form2";
             this.Text = "Търсене";
-            ((System.ComponentModel.ISupportInitialize)(this.DGrid2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView DGrid2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Намери;
     }
 }
