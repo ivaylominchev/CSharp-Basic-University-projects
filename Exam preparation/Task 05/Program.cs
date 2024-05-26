0668f7f0e59530c3f6cf2ce0527188156fbcf083
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-26 22:49:32 +0300

    finish

--- Exam preparation/Task 05/Program.cs
+++ Exam preparation/Task 05/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Въведете число:");
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+            Stack<char> uniqueStack = new Stack<char>();
+
+            foreach (char digit in input)
+            {
+                if (!stack.Contains(digit))
+                {
+                    stack.Push(digit);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                uniqueStack.Push(stack.Pop());
+            }
+
+            char[] resultArray = uniqueStack.ToArray();
+            string result = new string(resultArray);
+
+            Console.WriteLine("Резултат след премахване на повтарящите се цифри: " + result);
         }
     }
 }
