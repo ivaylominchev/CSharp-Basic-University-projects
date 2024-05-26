6a33da84c918e2d92ef8e44b0b33b353d1b47631
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-26 22:55:13 +0300

    finish

--- Exam preparation/Task 07/Program.cs
+++ Exam preparation/Task 07/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Stack<int> evenStack = new Stack<int>();
+            Stack<int> oddStack = new Stack<int>();
+
+            while (queue.Count > 0)
+            {
+                int number = queue.Dequeue();
+                if (number % 2 == 0)
+                {
+                    evenStack.Push(number);
+                }
+                else
+                {
+                    oddStack.Push(number);
+                }
+            }
+
+            Console.WriteLine("Четни стойности в стека:");
+            while (evenStack.Count > 0)
+            {
+                Console.WriteLine(evenStack.Pop());
+            }
+
+            Console.WriteLine("Нечетни стойности в стека:");
+            while (oddStack.Count > 0)
+            {
+                Console.WriteLine(oddStack.Pop());
+            }
         }
     }
 }
