19bbed0d915b9f51a623ea6299b868cf24f839d6
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-26 22:51:49 +0300

    finish

--- Exam preparation/Task 06/Program.cs
+++ Exam preparation/Task 06/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Въведете число:");
+            int number = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            if (number == 0)
+            {
+                stack.Push(0);
+            }
+            else
+            {
+                while (number > 0)
+                {
+                    int remainder = number % 2;
+                    stack.Push(remainder);
+                    number /= 2;
+                }
+            }
+
+            Console.Write("Двоично представяне: ");
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
         }
     }
 }
