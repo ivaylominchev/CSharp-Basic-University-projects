1d70d8633d2662ac9b05720dd26c8944ebc87c0d
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-26 22:59:46 +0300

    finish

--- Exam preparation/Task 11/Program.cs
+++ Exam preparation/Task 11/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Примерна редица
+            int[] array = { 34, 3, 31, 98, 92, 23 };
+
+            // Извеждаме оригиналната редица
+            Console.WriteLine("Оригинална редица:");
+            PrintArray(array);
+
+            // Сортираме редицата с помощта на опашка
+            int[] sortedArray = SortUsingQueue(array);
+
+            // Извеждаме сортираната редица
+            Console.WriteLine("Сортирана редица:");
+            PrintArray(sortedArray);
+        }
+
+        static int[] SortUsingQueue(int[] array)
+        {
+            Queue<int> queue = new Queue<int>(array);
+            List<int> sortedList = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                // Намерете минималния елемент в опашката
+                int min = int.MaxValue;
+                int n = queue.Count;
+
+                for (int i = 0; i < n; i++)
+                {
+                    int value = queue.Dequeue();
+                    if (value < min)
+                    {
+                        min = value;
+                    }
+                    queue.Enqueue(value);
+                }
+
+                // Извадете всички елементи, които са равни на минималния, и ги добавете към сортираната листа
+                for (int i = 0; i < n; i++)
+                {
+                    int value = queue.Dequeue();
+                    if (value != min)
+                    {
+                        queue.Enqueue(value);
+                    }
+                    else
+                    {
+                        sortedList.Add(value);
+                    }
+                }
+            }
+
+            return sortedList.ToArray();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
