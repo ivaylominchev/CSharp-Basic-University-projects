d37fafc6ee3a435f0a43a295e459606264871863
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-26 22:58:41 +0300

    finish

--- Exam preparation/Task 10/Program.cs
+++ Exam preparation/Task 10/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 34, 3, 31, 98, 92, 23 };
+
+            Console.WriteLine("Оригинална редица:");
+            PrintArray(array);
+
+            int[] sortedArray = SortUsingStacks(array);
+
+            Console.WriteLine("Сортирана редица:");
+            PrintArray(sortedArray);
+        }
+
+        static int[] SortUsingStacks(int[] array)
+        {
+            Stack<int> inputStack = new Stack<int>(array);
+            Stack<int> tempStack = new Stack<int>();
+
+            while (inputStack.Count > 0)
+            {
+                int current = inputStack.Pop();
+
+                while (tempStack.Count > 0 && tempStack.Peek() > current)
+                {
+                    inputStack.Push(tempStack.Pop());
+                }
+
+                tempStack.Push(current);
+            }
+
+            int[] sortedArray = tempStack.ToArray();
+
+            return sortedArray;
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
