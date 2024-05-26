4c8fcef158fdd8ba6a1b91e84a173d33b36e1e52
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-05-26 22:46:08 +0300

    finish

--- Exam preparation/Task 02/Program.cs
+++ Exam preparation/Task 02/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            Queue<int> queue = new Queue<int>();
+
+            int numberOfElements = 20;
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                int randomNumber = random.Next(-10, 11);
+                queue.Enqueue(randomNumber);
+            }
+
+            Console.WriteLine("Елементите в опашката:");
+            foreach (int num in queue)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            List<int> positiveNumbers = new List<int>();
+
+            while (queue.Count > 0 && positiveNumbers.Count < 5)
+            {
+                int current = queue.Dequeue();
+                if (current > 0)
+                {
+                    positiveNumbers.Add(current);
+                }
+            }
+
+            if (positiveNumbers.Count > 0)
+            {
+                Console.WriteLine("Първите 5 положителни числа са:");
+                foreach (int num in positiveNumbers)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Няма положителни числа в опашката.");
+            }
         }
     }
 }
